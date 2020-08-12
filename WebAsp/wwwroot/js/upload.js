66e673b5d017b29b4f0e2a59fc13ffab6c9be89e
sceptlc <sceptlc@bk.ru>
2020-08-13 01:53:59 +0300

    shit doesnt work on localhost

--- WebAsp/wwwroot/js/upload.js
+++ WebAsp/wwwroot/js/upload.js
@@ -1,18 +1,26 @@
-const hostUrl = "/api/dxf";
+const hostUrl = "https://u1123042.plsk.regruhosting.ru/api/dxf";
 const fileInput = document.getElementById("file");
 
 fileInput.addEventListener("change", handleFiles, false);
 
-async function handleFiles() {
+function handleFiles() {
     canvas.style.display = 'block';
     var formData = new FormData();
     const file = fileInput.files[0];
 
     formData.append("file", file);
 
+
     try {
-        var response = await fetch(hostUrl, { method: "POST", body: formData });
-        data = await response.json();
+        fetch(hostUrl, { method: "POST", mode: "no-cors", body: formData })
+            .then(response => response.text())
+            .then(result => {
+                console.log('Success:', result);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
         init();
     } catch (e) {
         console.error("Some problems: ", e);
