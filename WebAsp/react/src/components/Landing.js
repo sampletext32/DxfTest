b607b7c6d0732c5800b24c774db4558a4d7e9080
sceptlc <sceptlc@bk.ru>
2020-09-24 11:21:22 +0300

    finished landing layout, not responsible

--- WebAsp/react/src/components/Landing.js
+++ WebAsp/react/src/components/Landing.js
@@ -1,7 +1,13 @@
 import React from 'react'
-import { AppContext } from '../AppContext'
+import { useContext } from 'react'
 import Slider from 'react-slick'
+import { AppContext } from '../AppContext'
+import { Card, Accordion, useAccordionToggle, AccordionContext } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMinus, faPlus, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { faYoutube, faVk, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import '../styles/Landing.scss'
+
 
 class Landing extends React.Component {
 
@@ -11,7 +17,10 @@ class Landing extends React.Component {
             <div>
                 <HeroSection />
                 <ProcessSection />
-                <MaterialsSection/>
+                <MaterialsSection />
+                <FAQSection />
+                <ContactsSection />
+                <FooterSection />
             </div>
 
         );
@@ -105,22 +114,22 @@ function SliderSection() {
             <div className="col-12">
                 <Slider {...settings}>
                     <div className="inner">
-                        <img src="/img/1.png" alt="Процесс работы"/>
+                        <img src="/img/1.png" alt="Процесс работы" />
                     </div>
                     <div className="inner">
-                        <img src="/img/2.png" alt="Процесс работы"/>
+                        <img src="/img/2.png" alt="Процесс работы" />
                     </div>
                     <div className="inner">
-                        <img src="/img/3.png" alt="Процесс работы"/>
+                        <img src="/img/3.png" alt="Процесс работы" />
                     </div>
                     <div className="inner">
-                        <img src="/img/1.png" alt="Процесс работы"/>
+                        <img src="/img/1.png" alt="Процесс работы" />
                     </div>
                     <div className="inner">
-                        <img src="/img/2.png" alt="Процесс работы"/>
+                        <img src="/img/2.png" alt="Процесс работы" />
                     </div>
                     <div className="inner">
-                        <img src="/img/3.png" alt="Процесс работы"/>
+                        <img src="/img/3.png" alt="Процесс работы" />
                     </div>
                 </Slider>
             </div>
@@ -130,71 +139,211 @@ function SliderSection() {
 
 function MaterialsSection() {
     return (
-        <section className="materials container">
-            <div className="row">
-                <div className="col-12">
-                    <h2>Материалы для лазерной резки</h2>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-6 col-lg-4 material-card">
-                    <div className="inner">
-                        <h3>Медь</h3>
-                        <p>
-                            Режем преимущественно тонкую листовую медь (до 3 мм), в том числе материал клиента. Медь обладает высокой 
-                            теплопроводностью, поэтому для получения качественного реза скорость резки не должна быть высокой.
-                        </p>
+        <section className="materials">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h2>Материалы для лазерной резки</h2>
                     </div>
                 </div>
-                <div className="col-6 col-lg-4 material-card">
-                    <div className="inner">
-                        <h3>Латунь</h3>
-                        <p>
-                            Режем преимущественно тонкую листовую латунь (до 3 мм), в том числе материал клиента. 
-                            Выполняем высокоточную резку латуни толщиной.На фото представлены изделия из латуни толщиной 0,2 мм.
-                        </p>
+                <div className="row">
+                    <div className="col-6 col-lg-4 material-card">
+                        <div className="inner">
+                            <h3>Медь</h3>
+                            <p>
+                                Режем преимущественно тонкую листовую медь (до 3 мм), в том числе материал клиента. Медь обладает высокой
+                                теплопроводностью, поэтому для получения качественного реза скорость резки не должна быть высокой.
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="col-6 col-lg-4 material-card">
-                    <div className="inner">
-                        <h3>Нержавейка</h3>
-                        <p>
-                            Лазерная резка нержавейки с применением азота позволяет получить неокисленную идеальную кромку. 
-                            Режем нержавеющую сталь до 3 мм. Раскрой высокоточный. Минимальная погрешность - 0,01 мм.
-                        </p>
+                    <div className="col-6 col-lg-4 material-card">
+                        <div className="inner">
+                            <h3>Латунь</h3>
+                            <p>
+                                Режем преимущественно тонкую листовую латунь (до 3 мм), в том числе материал клиента.
+                                Выполняем высокоточную резку латуни толщиной.На фото представлены изделия из латуни толщиной 0,2 мм.
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="col-6 col-lg-4 material-card">
-                    <div className="inner">
-                        <h3>Оцинковка</h3>
-                        <p>
-                            Резка оцинковки осуществляется при использовании азотной среды, 
-                            позволяющей исключить горение металла в момент резки. При этом не потребуется 
-                            дополнительных операций обработки кромок.
-                        </p>
+                    <div className="col-6 col-lg-4 material-card">
+                        <div className="inner">
+                            <h3>Нержавейка</h3>
+                            <p>
+                                Лазерная резка нержавейки с применением азота позволяет получить неокисленную идеальную кромку.
+                                Режем нержавеющую сталь до 3 мм. Раскрой высокоточный. Минимальная погрешность - 0,01 мм.
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="col-6 col-lg-4 material-card">
-                    <div className="inner">
-                        <h3>Аллюминий</h3>
-                        <p>
-                            Алюминий и его сплавы имеют особые теплофизические характеристики, что требует высокой мощности лазера. 
-                            Кромка после реза идеальна - нет заусенец и облоя. Детали на листе можно располагать на расстоянии 2 мм.
-                        </p>
+                    <div className="col-6 col-lg-4 material-card">
+                        <div className="inner">
+                            <h3>Оцинковка</h3>
+                            <p>
+                                Резка оцинковки осуществляется при использовании азотной среды,
+                                позволяющей исключить горение металла в момент резки. При этом не потребуется
+                                дополнительных операций обработки кромок.
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div className="col-6 col-lg-4 material-card">
-                    <div className="inner">
-                        <h3>Дерево</h3>
-                        <p>
-                            Лазерная резка дерева возможна на толщине до 20 мм в зависимости от вида древесины. 
-                            А лазерная гравировка по дереву не ограничена каким-либо видом.
-                        </p>
+                    <div className="col-6 col-lg-4 material-card">
+                        <div className="inner">
+                            <h3>Аллюминий</h3>
+                            <p>
+                                Алюминий и его сплавы имеют особые теплофизические характеристики, что требует высокой мощности лазера.
+                                Кромка после реза идеальна - нет заусенец и облоя. Детали на листе можно располагать на расстоянии 2 мм.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-6 col-lg-4 material-card">
+                        <div className="inner">
+                            <h3>Дерево</h3>
+                            <p>
+                                Лазерная резка дерева возможна на толщине до 20 мм в зависимости от вида древесины.
+                                А лазерная гравировка по дереву не ограничена каким-либо видом.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
     )
 }
+
+function FAQSection() {
+    var questions = [
+        { q: 'Что такое лазерная резка', a: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eveniet ut facere consequuntur, adipisci molestiae possimus ipsam voluptatem minima doloribus laborum, voluptas suscipit labore. Cupiditate necessitatibus eos perspiciatis minima possimus odio nam velit sequi consectetur, mollitia quo repudiandae. Nobis repudiandae voluptas dolorum eligendi a impedit soluta illo tenetur alias officia.' },
+        { q: 'Что такое лазерная резка', a: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eveniet ut facere consequuntur, adipisci molestiae possimus ipsam voluptatem minima doloribus laborum, voluptas suscipit labore. Cupiditate necessitatibus eos perspiciatis minima possimus odio nam velit sequi consectetur, mollitia quo repudiandae. Nobis repudiandae voluptas dolorum eligendi a impedit soluta illo tenetur alias officia.' },
+        { q: 'Что такое лазерная резка', a: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eveniet ut facere consequuntur, adipisci molestiae possimus ipsam voluptatem minima doloribus laborum, voluptas suscipit labore. Cupiditate necessitatibus eos perspiciatis minima possimus odio nam velit sequi consectetur, mollitia quo repudiandae. Nobis repudiandae voluptas dolorum eligendi a impedit soluta illo tenetur alias officia.' },
+        { q: 'Что такое лазерная резка', a: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eveniet ut facere consequuntur, adipisci molestiae possimus ipsam voluptatem minima doloribus laborum, voluptas suscipit labore. Cupiditate necessitatibus eos perspiciatis minima possimus odio nam velit sequi consectetur, mollitia quo repudiandae. Nobis repudiandae voluptas dolorum eligendi a impedit soluta illo tenetur alias officia.' },
+        { q: 'Что такое лазерная резка', a: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur eveniet ut facere consequuntur, adipisci molestiae possimus ipsam voluptatem minima doloribus laborum, voluptas suscipit labore. Cupiditate necessitatibus eos perspiciatis minima possimus odio nam velit sequi consectetur, mollitia quo repudiandae. Nobis repudiandae voluptas dolorum eligendi a impedit soluta illo tenetur alias officia.' }
+    ];
+    return (
+        <section className="faq container">
+            <div className="row">
+                <div className="col-12">
+                    <h2>Вопрос-ответ</h2>
+                </div>
+                <div className="col-12">
+                    <Accordion>
+                        {questions.map((item, i) =>
+                            <Card key={i + 1}>
+                                <Card.Header>
+                                    <FAQAccordionToggle eventKey={i + 1}>
+                                        {item.q}
+                                    </FAQAccordionToggle>
+                                </Card.Header>
+                                <Accordion.Collapse eventKey={i + 1}>
+                                    <Card.Body>
+                                        {item.a}
+                                    </Card.Body>
+                                </Accordion.Collapse>
+                            </Card>
+                        )
+                        }
+                    </Accordion>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+function FAQAccordionToggle({ children, eventKey, callback }) {
+    const currentEventKey = useContext(AccordionContext);
+
+    const decoratedOnClick = useAccordionToggle(
+        eventKey,
+        () => callback && callback(eventKey),
+    );
+
+    const isCurrentEventKey = currentEventKey === eventKey;
+
+    return (<div className="inner" onClick={decoratedOnClick}>
+        { children}
+        { isCurrentEventKey ? <FontAwesomeIcon icon={faMinus} /> : <FontAwesomeIcon icon={faPlus} />}
+    </div>);
+}
+
+function ContactsSection() {
+
+    return (
+
+        <section className="contact container">
+            <div className="row">
+
+                <div className="col-12">
+                    <h2>Контакты</h2>
+                </div>
+
+                <div className="col-5">
+                    <div className="caption">Адрес:</div>
+                    <div className="value">г. Москва, Красная Площадь, дом 1</div>
+                    <div className="caption">Телефон:</div>
+                    <div className="value">+7(999)999-99-99</div>
+                    <div className="caption">Email:</div>
+                    <div className="value">7cut@gmail.com</div>
+                    <div className="caption">Часы работы:</div>
+                    <div className="value">ежедневно с 8:00 до 22:00</div>
+                    <div className="caption">Соцсети:</div>
+                    <div className="value social-icons">
+                        <span><FontAwesomeIcon icon={faYoutube} /></span>
+                        <span><FontAwesomeIcon icon={faVk} /></span>
+                        <span><FontAwesomeIcon icon={faInstagram} /></span>
+                    </div>
+                    <button className="button get-cost">Заказать обратный звонок</button>
+
+                </div>
+
+                <div className="col-7">
+                    <iframe width="100%" height="530" frameborder="0" title="map"
+                        src="https://yandex.com/map-widget/v1/?um=constructor%3A3f315d6c301076ae14c3abfd9232413e2bf799624c8ba86be7d6c2344d7273bd&amp;source=constructor" >
+                    </iframe>
+                </div>
+            </div>
+        </section>
+
+
+    )
+
+}
+
+function FooterSection() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm">
+                        <div>ЛОГОТИП</div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="footer-link">ИП Иванов Иван Иванович </div>
+                        <div className="footer-link">ОГРНИП: 325171828473829</div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="footer-link">ИНН: 325234561782</div>
+                        <div className="footer-link">Дата присвоения ОГРНИП: 01.01.2011</div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="footer-link">
+                            <FontAwesomeIcon icon={faEnvelope} />
+                            7cut@gmail.com
+                        </div>
+                        <div className="footer-link">
+                            <FontAwesomeIcon icon={faMapMarkerAlt} />
+                            г. Москва, Красная Площадь, дом 1
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="social-icons">
+                            <span><FontAwesomeIcon icon={faYoutube} /></span>
+                            <span><FontAwesomeIcon icon={faVk} /></span>
+                            <span><FontAwesomeIcon icon={faInstagram} /></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+
 
 export default Landing;
